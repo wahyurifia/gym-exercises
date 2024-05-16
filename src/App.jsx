91bec5e9ce3104149d41fb960c94b6300af03cfc
WahyuRifia <wahyurifia927@gmail.com>
2024-05-16 23:43:45 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { Route, Routes } from 'react-router-dom'
+import './App.css'
+
+import { Box } from '@mui/material'
+import ExerciseDetail from './pages/ExerciseDetail'
+import Home from './pages/Home'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+
+
+const App = () => {
+  return (
+    <Box width="400px" sx={{width: {xl: '1488px'}}}>
+    <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/exercise/:id' element={<ExerciseDetail />} />
+      </Routes>
+      <Footer />
+    </Box>
+  )
+}
+
+export default App
---
+++ src/components/HeroBanner.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import {Box, Stack, Typography} from '@mui/material'
+
+const HeroBanner = () => {
+  return (
+    <div>HeroBanner</div>
+  )
+}
+
+export default HeroBanner
